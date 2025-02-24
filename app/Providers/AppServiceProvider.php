955642cca87bd97f713dad97c15fd8bcb30c6ff5
rcpassos <r.passos@outlook.pt>
2025-02-24 22:40:48 +0000

    feat: register app configs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureUrl();
+    }
+
+    private function configureCommands()
+    {
+        DB::prohibitDestructiveCommands(
+            App::isProduction()
+        );
+    }
+
+    private function configureUrl()
+    {
+        // force https (for local developement, use -> valet secure <project-name>)
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
